a332c90c01aab01d0fe654c784ae89357b914081
taka-tuos <hvcnes@gmail.com>
2017-09-06 14:05:40 +0900

    2017-09-06

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 
 #include "Gtexture.h"
@@ -61,16 +62,16 @@ void sysProcessEvents(void)
 //----------------------------------------------------------------------
 
 Gtruetype *text;
+Gtruetype *textbig;
 Gtruetype *textmini;
 Gtexture *symbol;
-Gtexture *winnin;
+Gtexture *winnin,*winnin2;
 Gtexture *bg;
-Gtexture *logo;
+Gtexture *logo,*logo2,*mie;
 Gtexture *throwgun;
 
 uint32_t frame = 0;
 
-const float win_scale = 1.0f;
 const float win_speed = 4.0f;
 
 void VSRotate2D(float *xp, float *yp, float x, float y, float xc, float yc, float theta)
@@ -98,8 +99,6 @@ void VS30_Rotate(float u, float v, float x, float y, float w, float h, VertexSha
 
 void VS30_Scale(float u, float v, float x, float y, float w, float h, VertexShader_Data *ctx, void *ext)
 {
-	w *= win_scale;
-	h *= win_scale;
 	glTexCoord2f(0, 0); glVertex2f(x	, y		);
 	glTexCoord2f(0, v);	glVertex2f(x	, y + h);
 	glTexCoord2f(u, v);	glVertex2f(x + w, y + h);
@@ -117,9 +116,9 @@ void VS30_Width(float u, float v, float x, float y, float w, float h, VertexShad
 	glTexCoord2f(u, 0);	glVertex2f(x + w, y		);
 }
 
-float wx,wy,wh,ww,wvx,wvy;
+float wx[3],wy[3],wh[3],ww[3],wvx[3],wvy[3];
 
-void gfxDrawScreen(void)
+void gfxDrawScreen(int mode)
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -128,13 +127,82 @@ void gfxDrawScreen(void)
 	
 	bg->Draw(0,0);
 	
-	logo->Draw(1024-180-32,16);
+	if(mode == 0) {
+		logo->Draw(1024-180-32,16);
+		
+		symbol->Draw(1024/2-symbol->texture_sz[2]/2,0);
 	
-	symbol->Draw(1024/2-symbol->texture_sz[2]/2,0);
+		throwgun->Draw(0,600-170);
+		
+		winnin->Draw(wx[0],wy[0]);
+		
+		wy[0] += wvy[0];
+		wx[0] += wvx[0];
+		
+		if(wx[0] > 1024 - winnin->texture_sz[2] || wx[0] < 0) wvx[0] = -wvx[0];
+		if(wy[0] > 600  - winnin->texture_sz[3] || wy[0] < 0) wvy[0] = -wvy[0];
+	}
 	
-	throwgun->Draw(0,600-170);
+	if(mode == 1) {
+		winnin->Draw(1024-180-32,16);
+		
+		logo2->Draw(1024/2-logo2->texture_sz[2]/2,50);
 	
-	winnin->Draw(wx,wy);
+		throwgun->Draw(0,600-170);
+		
+		logo->Draw(wx[1],wy[1]);
+		
+		wy[1] += wvy[1];
+		wx[1] += wvx[1];
+		
+		if(wx[1] > 1024 - logo->texture_sz[2] || wx[1] < 0) wvx[1] = -wvx[1];
+		if(wy[1] > 600  - logo->texture_sz[3] || wy[1] < 0) wvy[1] = -wvy[1];
+	}
+	
+	if(mode == 2) {
+		struct tm t;
+	
+		t.tm_year = 118;     /* 2001年 */
+		t.tm_mon  = 6;       /* 5月    */
+		t.tm_mday = 26;      /* 20日   */
+		t.tm_wday = 4;       /* 日曜日 */
+		t.tm_hour = 0;      /* 14時   */
+		t.tm_min  = 0;      /* 20分   */
+		t.tm_sec  = 0;       /* 00秒   */
+		t.tm_isdst= -1;      /* 夏時間無効 */
+		
+		long days = ((mktime(&t) - time(NULL))+86399)/86400;
+		
+		logo->Draw(1024-180-32,16);
+		
+		mie->Draw(1024/2-mie->texture_sz[2]/2,100);
+	
+		throwgun->Draw(0,600-170);
+		
+		winnin2->Draw(wx[2],wy[2]);
+		
+		char s[30];
+	
+		sprintf(s,"あと%d日",days);
+		
+		glColor4f(0, 0, 0, 1.0);
+		
+		for(int i = -1; i < 2; i++)
+			for(int j = -1; j < 2; j++)
+				if(i && j) textbig->Draw(64+i,64+j,s,1);
+		
+		glColor4f(1, 0.5, 0, 1.0);
+		
+		textbig->Draw(64,64,s,1);
+		
+		wy[2] += wvy[2];
+		wx[2] += wvx[2];
+		
+		wvy[2]++;
+		
+		if(wx[2] > 1024 - winnin2->texture_sz[2] || wx[2] < 0) wvx[2] = -wvx[2];
+		if(wy[2] > 600  - winnin2->texture_sz[3]) wvy[2] = -30;
+	}
 	
 	glColor4f(0, 0, 0, 1.0);
 	
@@ -143,12 +211,6 @@ void gfxDrawScreen(void)
 	text->Draw(64,16,"2018 彩る感動 東海総体",1);
 	
 	frame++;
-	
-	wy += wvy;
-	wx += wvx;
-	
-	if(wx > 1024 - ww || wx < 0) wvx = -wvx;
-	if(wy > 600  - wh || wy < 0) wvy = -wvy;
 	
 	SDL_GL_SwapBuffers();
 }
@@ -213,17 +275,24 @@ int main(int argc, char* argv[])
 	gfxInitialize(width, height);
 	
 	text = new Gtruetype("衡山毛筆フォント.ttf");
+	textbig = new Gtruetype("衡山毛筆フォント.ttf");
 	textmini = new Gtruetype("circle-mplus-1p-regular.ttf");
 	symbol = new Gtexture;
 	winnin = new Gtexture;
+	winnin2 = new Gtexture;
 	throwgun = new Gtexture;
 	bg = new Gtexture;
 	logo = new Gtexture;
+	logo2 = new Gtexture;
+	mie = new Gtexture;
 	
 	symbol->Load("symbol.png");
 	symbol->AttachVertexShader(VS30_Rotate);
 	
 	winnin->Load("winnin-test.png");
+	
+	winnin2->Load("winnin2.png");
+	//w2->AttachVertexShader(VS30_Rotate);
 	
 	throwgun->Load("throwgun.png");
 	
@@ -231,20 +300,34 @@ int main(int argc, char* argv[])
 	
 	logo->Load("interlogo.png");
 	
+	logo2->Load("interlogo2.png");
+	logo2->AttachVertexShader(VS30_Rotate);
+	
+	mie->Load("mie.png");
+	mie->AttachVertexShader(VS30_Rotate);
+	
 	text->Size(48);
+	textbig->Size(128);
 	textmini->Size(16);
 	
-	ww = winnin->texture_sz[2] * win_scale;
-	wh = winnin->texture_sz[3] * win_scale;
-	wx = fmod((float)rand(),1024 - ww);
-	wy = fmod((float)rand(),600 - wh);
+	Gtexture *list[3];
 	
-	wvx = (rand() & 1) ? win_speed : -win_speed;
-	wvy = (rand() & 1) ? win_speed : -win_speed;
+	list[0] = winnin;
+	list[1] = logo;
+	list[2] = winnin2;
+	
+	for(int i = 0; i < 3; i++) {
+		wx[i] = fmod((float)rand(),1024 - list[i]->texture_sz[2]);
+		wy[i] = fmod((float)rand(),600 - list[i]->texture_sz[3]);
+		
+		wvx[i] = (rand() & 1) ? win_speed : -win_speed;
+		wvy[i] = (rand() & 1) ? win_speed : -win_speed;
+	}
 
 	while(1) {
+		time_t time_now=time(NULL);
 		sysProcessEvents();
-		gfxDrawScreen();
+		gfxDrawScreen((time_now / 20UL) % 3UL);
 	}
 	
 	return 0;
